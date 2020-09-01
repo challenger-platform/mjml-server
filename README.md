3538e74fd19c9e0db39d7b62869b75b3503fbd7b
Tomas Stan <tomas.stan@enga.systems>
2020-09-01 22:35:28 +0300

    Dropped MJML3 support

--- README.md
+++ README.md
@@ -12,10 +12,10 @@
 
 ### MJML version 3
 ```bash
-curl -X POST "http://127.0.0.1:3000/v3" --header "Content-Type: application/json" -d '{"mjml":"<mjml><mj-body><mj-container><mj-section><mj-column><mj-text>Hello World</mj-text></mj-column></mj-section></mj-container></mj-body></mjml>"}'
+curl -X POST "http://127.0.0.1:3000" --header "Content-Type: application/json" -d '{"mjml":"<mjml><mj-body><mj-container><mj-section><mj-column><mj-text>Hello World</mj-text></mj-column></mj-section></mj-container></mj-body></mjml>"}'
 ```
 
 ### MJML version 4
 ```bash
-curl -X POST "http://127.0.0.1:3000/v4" --header "Content-Type: application/json" -d '{"mjml":"<mjml><mj-body><mj-container><mj-section><mj-column><mj-text>Hello World</mj-text></mj-column></mj-section></mj-container></mj-body></mjml>"}'
+curl -X POST "http://127.0.0.1:3000" --header "Content-Type: application/json" -d '{"mjml":"<mjml><mj-body><mj-section><mj-column><mj-text>Hello World!</mj-text></mj-column></mj-section></mj-body></mjml>"}'
 ```
--- index.js
+++ index.js
@@ -1,6 +1,5 @@
 const fastify = require('fastify')()
-const mjml3 = require('mjml3')
-const mjml4 = require('mjml4')
+const mjml2html = require('mjml')
 const dayjs = require('dayjs')
 
 const port = 3000
@@ -29,31 +28,21 @@ let processPost = function (request, reply) {
     return;
   }
 
-  // Parse MJML4
-  if(url.pathname == '/v4'){
-    let result = mjml4(request.body.mjml, {
-      minify: true,
-    });
+  let result = mjml2html(request.body.mjml, {
+    minify: true,
+  })
 
-    console.log(result);
-    reply.send(result.html)
-  }else{ // Parse MJML3 by default
-    let result = mjml3.mjml2html(request.body.mjml);
-
-    if (Object.keys(result.errors).length) {
-      Object.keys(result.errors).forEach((key) => {
-        delete result.errors[key].formattedMessage
-      })
-    }
-
-    reply.send(result)
+  if (Object.keys(result.errors).length) {
+    Object.keys(result.errors).forEach((key) => {
+      delete result.errors[key].formattedMessage
+    })
   }
+
+  reply.send(result)
 }
 
 // Two URL's
 fastify.post('/', processPost)
-fastify.post('/v3', processPost)
-fastify.post('/v4', processPost)
 
 // Run the server!
 fastify.listen(port, '127.0.0.1', function (err) {
