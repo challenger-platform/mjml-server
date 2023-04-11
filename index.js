13d51bc539c05c398f7e784a332d1854f33e64aa
Tomas Stan <tomastan@enga.systems>
2023-04-11 15:53:15 +0300

    Force using local path for .mjmlconfig

--- index.js
+++ index.js
@@ -6,40 +6,40 @@ const port = 3000
 const host = "0.0.0.0" // Use "127.0.0.1" to accept connections from local interface only
 
 const log = {
-  error (text) {
-    console.log("[" + dayjs().format('YYYY-MM-DD HH:mm:ss') + "] Error: " + text)
-  },
+	error (text) {
+		console.log("[" + dayjs().format('YYYY-MM-DD HH:mm:ss') + "] Error: " + text)
+	},
 
-  notice (text) {
-    console.log("[" + dayjs().format('YYYY-MM-DD HH:mm:ss') + "] Notice: " + text)
-  }
+	notice (text) {
+		console.log("[" + dayjs().format('YYYY-MM-DD HH:mm:ss') + "] Notice: " + text)
+	}
 }
 
 // Processing HTTP request
 let processPost = (request, reply) => {
-  // let url = new URL(request.raw.url, 'http://127.0.0.1:' + port)
+	// let url = new URL(request.raw.url, 'http://127.0.0.1:' + port)
 
-  if (!request.body || typeof request.body.mjml === 'undefined' || request.body.mjml === null) {
-    reply.send({
-      error: "No MJML input"
-    })
+	if (!request.body || typeof request.body.mjml === 'undefined' || request.body.mjml === null) {
+		reply.send({
+			error: "No MJML input"
+		})
 
-    log.error("Received empty request body")
+		log.error("Received empty request body")
 
-    return;
-  }
+		return;
+	}
 
-  let result = mjml2html(request.body.mjml, {
-    // minify: true, // minify option is deprecated in latest MJML 4
-  })
+	let result = mjml2html(request.body.mjml, {
+		mjmlConfigPath: __dirname, // Set mjml config path to script root directory
+	})
 
-  if (Object.keys(result.errors).length) {
-    Object.keys(result.errors).forEach((key) => {
-      delete result.errors[key].formattedMessage
-    })
-  }
+	if (Object.keys(result.errors).length) {
+		Object.keys(result.errors).forEach((key) => {
+			delete result.errors[key].formattedMessage
+		})
+	}
 
-  reply.send(result)
+	reply.send(result)
 }
 
 // Two URL's
@@ -47,7 +47,7 @@ fastify.post('/', processPost)
 
 // Run the server!
 fastify.listen(port, host, (err) => {
-  if (err) throw err;
+	if (err) throw err;
 
-  log.notice(`server listening on ${fastify.server.address().port}`)
+	log.notice(`server listening on ${fastify.server.address().port}`)
 })
